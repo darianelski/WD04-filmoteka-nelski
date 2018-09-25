0c8b5e7a11b0f5d25bc5f1ba30ee10419d1c86ab
Daria Nelski <daria.nelski@gmail.com>
2018-09-25 15:43:37 +0300

    test

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php 
 
 function db_connect() {
-	$link = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD,MYSQL_DB); 
+	$link = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB); 
 
 	if (mysqli_connect_error()) {
 		die("Ошибка подключения к базе данных.");
